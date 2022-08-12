4f5cbd3473a66a77c25002db2e43836052f61c67
Kevin Kipkoech <kevin.kipkoech@student.moringaschool.com>
2022-08-12 22:13:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    "name": "Kevin Kipkoech",
+    "streetAddress": "15 Zumaridi Estate, Thika"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newNewEMployee = {...employee};
+    delete newNewEMployee[key];
+    return newNewEMployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
